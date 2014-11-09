6944667e807262bc33541bcf029effb8180e29da
ericzyh <8040334@qq.com>
2014-11-09 10:40:20 +0800

    init project

---
+++ swiftgame1/swiftgame1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftgame1
+//
+//  Created by 赵宇豪 on 14/11/9.
+//  Copyright (c) 2014年 zhaoyuhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
